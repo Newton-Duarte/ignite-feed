2236b0b7436caf3921d728109cf1e3e7f42f8dc2
Newton Duarte <newton_@hotmail.com>
2024-01-25 19:18:49 -0300

    feat: add comment component

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,3 +1,5 @@
+import { Comment } from '../Comment'
+
 import styles from './Post.module.css';
 
 export function Post() {
@@ -37,6 +39,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentsList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
