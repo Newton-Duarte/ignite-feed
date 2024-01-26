600440e936c72bb18ee7221dcf5817a5f083424b
Newton Duarte <newton_@hotmail.com>
2024-01-26 12:10:35 -0300

    feat: use avatar component

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,11 +1,13 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from '../Avatar';
+
 import styles from './Comment.module.css';
 
-import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/newton-duarte.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/newton-duarte.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
